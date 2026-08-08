9722f740a2e52597760a5082cd8a536d36120f40
Morgan Slatter <morganpgcontact@gmail.com>
2026-08-08 01:16:57 +0100

    add description to main.py and add usage guide if no arguments are parsed

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+'''
+DMG Gameboy Emulator Project - MorganPG
+
+main.py:
+
+The main script, this is what you execute to run the code.
+'''
 from board.board import Motherboard
 from sys import argv
 
 SYMBOLS = ""
-if len(argv) > 2:
+if len(argv) == 1:
+    print("Usage: main.py <gameboy rom> <optional debug symbols file>")
+    exit()
+elif len(argv) > 2:
     SYMBOLS = argv[2]
 
 CART = argv[1]
